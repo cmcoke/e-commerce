74248764d4f67d4a356ee499cba21171f6434f64
cmcoke <minoru.cm@gmail.com>
2023-12-04 14:50:22 -0500

    created the footer section.

--- src/app/_components/Button/index.tsx
+++ src/app/_components/Button/index.tsx
@@ -16,7 +16,7 @@ export type Props = {
   type?: 'submit' | 'button'
   disabled?: boolean
   invert?: boolean
-  childern?: React.ReactNode
+  children?: React.ReactNode
 }
 
 export const Button: React.FC<Props> = ({
@@ -30,7 +30,7 @@ export const Button: React.FC<Props> = ({
   type = 'button',
   disabled,
   invert,
-  childern,
+  children,
 }) => {
   let el = elFromProps
 
@@ -48,7 +48,7 @@ export const Button: React.FC<Props> = ({
   const content = (
     <div className={classes.content}>
       <span className={classes.label}>{label}</span>
-      {childern}
+      {children}
     </div>
   )
 
